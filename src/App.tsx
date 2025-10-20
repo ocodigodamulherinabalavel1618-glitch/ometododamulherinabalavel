f43af5ff92f4a53d6902950ea616dbf9af84ba8d
pedrovasconcellos-hue <ocodigodamulherinabalavel1618@gmail.com>
2025-10-19 21:47:44 -0300

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -94,7 +94,7 @@ function App() {
         <div className="relative max-w-5xl mx-auto text-center space-y-8">
           {/* Headline */}
           <h1 className="text-3xl md:text-5xl lg:text-6xl font-serif font-bold leading-tight text-gray-100">
-            Este é o <span className="text-gradient-gold">segredo para reprogramar</span> a sua mente.
+            Você Vai Matar a Parte de Você Que Te Sabota ou Vai Deixar Ela <span className="text-gradient-gold">Matar Seus Sonhos </span> de Novo?”
           </h1>
 
           {/* Subheadline */}
